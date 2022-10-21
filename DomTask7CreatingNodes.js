e7b9933fec09e8b30acd477af0e395d7b180affc
Nizamph <nizamph7@gmail.com>
2022-10-21 16:38:03 +0530

    Task7-Dom manipulation-CreatingNodes-JsFile

---
+++ DomTask7CreatingNodes.js
@@ -0,0 +1,91 @@
+//TRVERSING THE NODE:-
+ var itemList = document.querySelector('#items');
+ //parentNode
+ console.log(itemList.parentNode);
+ itemList.parentNode.style.backgroundColor = '#f4f4f4';
+ console.log(itemList.parentNode.parentNode.parentNode);
+
+//parentElement
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = 'blue';
+console.log(itemList.parentElement.parentElement.parentElement)
+
+//childNodes
+console.log(itemList.childNodes);//give nodes 
+
+console.log(itemList.children);//give html collection of elements
+
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = 'yellow'
+
+//firstChild
+console.log(itemList.firstChild)//it will give #text coz </li> and starting of the next line's <li> is arranged in seperate row 
+//firstElementChild
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = 'hello 1'
+
+//lastChild
+console.log(itemList.lastChild)
+//firstElementChild
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent = 'hello 4'
+
+// nextSibling
+console.log(itemList.nextSibling)
+//nextElementSibling
+console.log(itemList.nextElementSibling);
+
+// previousSibling
+console.log(itemList.previousSibling);
+// previousElementSibling:-
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color = 'green'
+
+//createElement
+
+//create a div
+
+var newDiv = document.createElement('div');
+//add class
+newDiv.classname = 'hello';
+
+//add Id
+newDiv.id = 'hello 1'
+console.log(newDiv);
+
+//add attribute
+newDiv.setAttribute('title', 'hello Div');
+
+//create text node
+var newDivText = document.createTextNode('Hello World')
+
+// add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px'
+container.insertBefore(newDiv, h1);
+
+//ADD HELLO WORLD BEFORE ITEM 1
+
+var newDiv = document.createElement('div');
+newDiv.className = 'hello 1';
+newDiv.id = 'hello 1';
+newDiv.setAttribute('title', 'hello Div');
+var newDivText = document.createTextNode('hello World');
+newDiv.appendChild(newDivText);
+
+var li = document.querySelector('li');
+console.log(li.parentNode);
+var itemlist = li.parentNode
+
+var item = document.querySelector('li:nth-child(1)')
+console.log(item);
+
+itemlist.insertBefore(newDiv,li)
+
+
